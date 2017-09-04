ddbdc33f218846927b18b24ee4fb3f15179c4976
nguyenhominhnhut <itnguyenhominhnhut@gmail.com>
2017-09-04 22:25:55 +0700

    CSS

--- application/libraries/template.php
+++ application/libraries/template.php
@@ -3,6 +3,7 @@ class Template {
 	private $CI;
 	private $template_data = array();
 	private $_scripts_header = array();
+	private $_styles = array();
 	public function __construct()
 	{
 		$this->CI =& get_instance();
@@ -12,28 +13,64 @@ class Template {
 	{
 		$this->template_data[$content_area] = $value;
 	}
-
-	public function add_js($src, $is_footer = FALSE)
-	{
-		if (!$is_footer) {
-			$this->_scripts_header[] = $this->script_tag($src);
-		} else {
-			$this->_scripts_footer[] = $this->script_tag($src);
+	 /**
+     * add css
+     *
+     * @param  string $path
+     * @param  string $media
+     * @return void
+     */
+	 public function add_css($href = NULL, $media = 'screen')
+	 {
+	 	$href = ltrim($href, "/");
+	 	$link = array(
+	 		'href' => $href,
+	 		'rel' => 'stylesheet',
+	 		'type' => 'text/css'
+	 		);
+	 	if (!empty($media)) {
+	 		$link['media'] = $media;
+	 	}
+	 	$this->_styles[] = link_tag($link);
+	 }
+     /**
+     * add js
+     * add script on header or footer (before </body>)
+     *
+     * @param string  $src
+     * @param boolean $is_footer
+     */
+		public function add_js($src, $is_footer = FALSE)
+		{
+			if (!$is_footer) {
+				$this->_scripts_header[] = $this->script_tag($src);
+			} else {
+				$this->_scripts_footer[] = $this->script_tag($src);
+			}
 		}
-	}
-	private function script_tag($src = NULL)
-	{
-		if (isset($src) and !empty($src)) {
-			return '<script src="' .base_url(). $src . '" type="text/javascript"></script>';
+		 /**
+     * script_tag
+     *
+     * Generates an javascript heading tag. First param is the data.
+     *
+     * @access private
+     * @param  string
+     * @return string
+     */
+		private function script_tag($src = NULL)
+		{
+			if (isset($src) and !empty($src)) {
+				return '<script src="' .base_url(). $src . '" type="text/javascript"></script>';
+			}
+			return "";
 		}
-		return "";
-	}
-	function load($template = '', $name ='', $view = '' , $view_data = array(), $return = FALSE)
-	{
-		$this->set($name , $this->CI->load->view($view, $view_data, TRUE));
-	  $this->set('scripts_header', implode("\r\n", $this->_scripts_header) . "\r\n");
-		$this->CI->load->view('layouts/font_end/'.$template, $this->template_data);
-	}
+		function load($template = '', $name ='', $view = '' , $view_data = array(), $return = FALSE)
+		{
+			$this->set($name , $this->CI->load->view($view, $view_data, TRUE));
+			$this->set('styles', implode("\r\n", $this->_styles) . "\r\n");
+		  $this->set('scripts_header', implode("\r\n", $this->_scripts_header) . "\r\n");
+			$this->CI->load->view('layouts/font_end/'.$template, $this->template_data);
+		}
 
 }
 ?>
--- application/views/layouts/font_end/layout.php
+++ application/views/layouts/font_end/layout.php
@@ -3,12 +3,14 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Test CI Application - <?php echo $title;?></title>
+	<?php echo $styles;?>
 </head>
 <body>
 	<?php echo $contents;?>
-	<!-- basic scripts -->
+
 	<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-3.2.1.min.js"></script>
 	<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+		<!-- basic scripts -->
 	<?php echo $scripts_header;?>
 </body>
 </html>
